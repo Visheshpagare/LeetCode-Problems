2ff7f7372bf4366f1e2ab2bd95fd49c9df032777
Visheshpagare <visheshhpagare2024@gmail.com>
2026-06-23 23:21:53 +0530

    Time: 136 ms (5.85%), Space: 46.9 MB (5.07%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -0,0 +1,28 @@
+class Solution {
+
+public void main(String args[]){
+    String s = "{}";
+    System.out.println(isValid(s));
+}
+
+    public boolean isValid(String s) {
+        while(true){
+            if(s.contains("()")){
+                s = s.replace("()" , "");
+            }
+            
+            else if(s.contains("{}")){
+                s = s.replace("{}" , "");
+            }
+            
+            else if(s.contains("[]")){
+                s = s.replace("[]" , "");
+            }
+            else {
+                return s.isEmpty();
+            }
+        }
+
+        }
+        
+    }
