84941ab4ea72ae1d4e019cb1c54c6473738bf609
Visheshpagare <visheshhpagare2024@gmail.com>
2026-06-24 12:32:47 +0530

    Time: 0 ms (100%), Space: 41.9 MB (85.91%) - LeetHub

---
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int climbStairs(int n) {
+
+        int a = 3;
+        int b = 2;
+        if(n <= 3){
+            return n;
+        }
+        for(int i =0; i<n-3 ; i++){
+            a = a+b;
+            b = a-b;
+        }
+        return a;
+        
+    }
+}
